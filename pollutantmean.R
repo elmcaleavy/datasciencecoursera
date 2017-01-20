d0080dc9862dfefca66b18045e68157360c42d1e
elmcaleavy <emma.mcaleavy@gmail.com>
2017-01-19 22:50:14 -0500

    Add files via upload

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,10 @@
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-        filename <- paste (formatC(id, width = 3, format = "d", flag = "0"), ".csv", sep = "")
-        path <- file.path(directory, filename)
-        data <- lapply(path, read.csv)
-        combineddata <- do.call(rbind, data)
-        mean(combineddata[, pollutant], na.rm = TRUE)
-        
+pollutantmean <- function (directory, pollutant, id = 1:332) {
+        allfiles <- file.path(directory, list.files(directory))
+        collector <- data.frame(date = factor(), sulfate = numeric(), nitrate = numeric(), id = integer())
+        for (file in allfiles[id]) {
+               collector <- rbind(collector, read.csv(file))
+        }
+        mean (collector[, pollutant], na.rm = TRUE)
+
 }
+
