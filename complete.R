11e06420d809934f819f3e45ea46d6e7a24d2491
elmcaleavy <emma.mcaleavy@gmail.com>
2017-01-22 19:33:46 -0500

    Add files via upload

--- complete.R
+++ complete.R
@@ -1,12 +1,16 @@
 complete <- function(directory, id = 1:332) {
-        filename <- paste (formatC(id, width = 3, format = "d", flag = "0"), ".csv", sep = "")
-        path <- file.path(directory, filename)
-        data <- lapply(path, read.csv)
-        completedata <- lapply(data, complete.cases)
-        truesum <- lapply (completedata, sum)
-        nobs <- unlist(truesum)
-        data.frame(id, nobs)
-       
+        allfiles <- file.path(directory, list.files(directory))
+        output <- data.frame(id = integer(), nobs = numeric())
+        
+        for (current_id in id) {
+                file <- allfiles[current_id]
+                data <- read.csv(file)
+                nobs <- sum(complete.cases(data))
+                newrow <- data.frame(id = current_id, nobs = nobs)
+                output <- rbind(output, newrow)
+                
+        }
+        output
 }
 
 
